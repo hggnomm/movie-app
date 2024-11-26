14848dadfb9cc79b3bd7f5e09fbf1e811f0e6495
hggnomm <hggnomm.dev@gmail.com>
2024-11-26 08:51:30 +0700

    movie code

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,30 +1,38 @@
 import React from "react";
 import "./Card.css";
-const Card = ({ cardWidth }) => {
+const Card = ({ movie, cardWidth }) => {
+  const { title, image, genres, originalLanguage, releaseDate, overview } =
+    movie;
   return (
     <div
       style={{ width: cardWidth }}
-      className="h-[650px] relative flex justify-center items-center shrink-0 p-2 bg-gray-600 group"
+      className="h-[650px] relative flex justify-center items-center shrink-0 p-2 bg-gray-600 cursor-pointer group"
     >
       <div className="w-[97%] h-[97%] m-auto text-white absolute rounded-lg bg-black/50 flex flex-col justify-center gap-y-2 p-10 cursor-pointer">
-        <h1 className="text-4xl">The Godfather</h1>
+        <h1 className="text-4xl">{title}</h1>
         <div className="flex gap-x-2 items-center">
           <span className="text-lg">Genres:</span>
-          <span className="font-semibold text-red-600">Crime Drama</span>
+          {genres.map((genre, index) => (
+            <span key={index} className="font-semibold text-red-600">
+              {genre}
+            </span>
+          ))}
         </div>
         <span className="flex gap-x-2">
-          Original Language: <span className="mr-2 uppercase">EN</span>
+          Original Language:{" "}
+          <span className="mr-2 uppercase">{originalLanguage}</span>
         </span>
         <span className="flex gap-x-2">
-          Release Data: <span className="mr-2 text-yellow-400">1972-03-04</span>
+          Release Data:{" "}
+          <span className="mr-2 text-yellow-400">{releaseDate}</span>
         </span>
         <p className="flex flex-col gap-y-1">
           <span className="text-red-500">Summary: </span>
-          <span className="first-letter:pl-2">Summary</span>
+          <span className="first-letter:pl-2">{overview}</span>
         </p>
       </div>
       <img
-        src="/public/img.png"
+        src={image}
         alt="Movie Image"
         className="absolute w-[97%] h-[97%] object-cover rounded-xl opacity-50 group-hover:opacity-100 transition-opacity duration-500"
       />
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Card from "../Card/Card";
 const Home = () => {
   const [cardWidth, setCardWidth] = useState(500);
   const cardsInRow = 5;
   const [wrapperWidth, setWrapperWidth] = useState(cardWidth * cardsInRow);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const getMovies = async () => {
+    const url =
+      "https://tvshow.p.rapidapi.com/Movie/NowPlaying?Page=1&Language=en-US&Adult=true";
+    const options = {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "53d61623b7msh44356d39f002645p1689fcjsn745cf05f1c23",
+        "x-rapidapi-host": "tvshow.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setMovies(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div
       className="flex justify-center items-center"
       style={{ width: wrapperWidth }}
     >
       <div className="flex flex-wrap">
-        <div>
-          <Card cardWidth={cardWidth} />
-        </div>
+        {movies.map((movie, index) => (
+          <div key={index}>
+            <Card movie={movie} cardWidth={cardWidth} />
+          </div>
+        ))}
       </div>
     </div>
   );
