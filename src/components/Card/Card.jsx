e80495cd9b3330929cb30e18f4527b05798187f3
hggnomm <hggnomm.dev@gmail.com>
2024-11-26 17:16:52 +0700

    card informain

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
+import { motion } from "framer-motion";
 const Card = ({ movie, cardWidth }) => {
   const { title, image, genres, originalLanguage, releaseDate, overview } =
     movie;
+  const [showDesc, setShowDesc] = useState(false);
   return (
     <div
       style={{ width: cardWidth }}
-      className="h-[650px] relative flex justify-center items-center shrink-0 p-2 bg-gray-600 cursor-pointer group"
+      className="h-[650px] relative flex justify-center items-center shrink-0 p-2 group"
     >
-      <div className="w-[97%] h-[97%] m-auto text-white absolute rounded-lg bg-black/50 flex flex-col justify-center gap-y-2 p-10 cursor-pointer">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: showDesc ? 1 : 0 }}
+        transition={{ duration: 0.2 }}
+        onClick={() => setShowDesc(!showDesc)}
+        className="w-[97%] h-[97%] m-auto text-white absolute rounded-lg bg-black/50 flex flex-col justify-center gap-y-2 p-10 cursor-pointer backdrop-blur-2xl"
+      >
         <h1 className="text-4xl">{title}</h1>
         <div className="flex gap-x-2 items-center">
           <span className="text-lg">Genres:</span>
@@ -30,11 +38,11 @@ const Card = ({ movie, cardWidth }) => {
           <span className="text-red-500">Summary: </span>
           <span className="first-letter:pl-2">{overview}</span>
         </p>
-      </div>
+      </motion.div>
       <img
         src={image}
         alt="Movie Image"
-        className="absolute w-[97%] h-[97%] object-cover rounded-xl opacity-50 group-hover:opacity-100 transition-opacity duration-500"
+        className="absolute w-[97%] h-[97%] object-cover rounded-xl opacity-50 group-hover:opacity-100 transition-opacity duration-500 -z-10"
       />
     </div>
   );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Card from "../Card/Card";
+import Navigation from "../Navitation/Navigation";
 const Home = () => {
   const [cardWidth, setCardWidth] = useState(500);
   const cardsInRow = 5;
   const [wrapperWidth, setWrapperWidth] = useState(cardWidth * cardsInRow);
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [group, setGroup] = useState("Popular");
 
   useEffect(() => {
     getMovies();
-  }, []);
+  }, [page, group]);
 
   const getMovies = async () => {
-    const url =
-      "https://tvshow.p.rapidapi.com/Movie/NowPlaying?Page=1&Language=en-US&Adult=true";
+    const url = `https://${
+      import.meta.env.VITE_BASE_URL
+    }/Movie/${group}?Page=${page}&Language=en-US&Adult=true`;
     const options = {
       method: "GET",
       headers: {
-        "x-rapidapi-key": "53d61623b7msh44356d39f002645p1689fcjsn745cf05f1c23",
-        "x-rapidapi-host": "tvshow.p.rapidapi.com",
+        "x-rapidapi-key": import.meta.env.VITE_X_RAPIAPI_KEY,
+        "x-rapidapi-host": import.meta.env.VITE_BASE_URL,
       },
     };
 
@@ -31,18 +35,21 @@ const Home = () => {
     }
   };
   return (
-    <div
-      className="flex justify-center items-center"
-      style={{ width: wrapperWidth }}
-    >
-      <div className="flex flex-wrap">
-        {movies.map((movie, index) => (
-          <div key={index}>
-            <Card movie={movie} cardWidth={cardWidth} />
-          </div>
-        ))}
+    <>
+      <Navigation page={page} setPage={setPage} setGroup={group} />
+      <div
+        className="flex justify-center items-center"
+        style={{ width: wrapperWidth }}
+      >
+        <div className="flex flex-wrap">
+          {movies.map((movie, index) => (
+            <div key={index}>
+              <Card movie={movie} cardWidth={cardWidth} />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
